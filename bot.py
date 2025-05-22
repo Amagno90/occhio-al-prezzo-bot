15184fa7432c4f2d1028909e69c04177c8c1e665
Amagno90 <96139295+Amagno90@users.noreply.github.com>
2025-05-22 16:01:50 +0200

    uso variabile ambiente per token

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # CONFIGURAZIONE
-BOT_TOKEN = '7720635859:AAH9vY_n7ozctVMo_llGvw37AFbtZpWhQ78'
+BOT_TOKEN = os.getenv("BOT_TOKEN"))
 CHANNEL_ID = '@occhioalprezzo'
 AFFILIATE_ID = 'occhioalpre08-21'
 RSS_FEED = 'https://www.amazon.it/gp/rss/bestsellers/electronics/'
