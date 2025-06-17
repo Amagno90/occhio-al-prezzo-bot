3e8b47eb8e59a42bc4efcb618cee797b60d072e1
Amagno90 <96139295+Amagno90@users.noreply.github.com>
2025-06-17 14:51:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Bot
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # già configurato
-CHANNEL_ID = "@nomecanale"  # sostituisci con il tuo canale
+CHANNEL_ID = "@OcchioalP"  # sostituisci con il tuo canale
 
 bot = Bot(token=BOT_TOKEN)
 
